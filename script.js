49297f9af706eef50f661794aef96d824d5f16f2
Velrox <maxcodes99@gmail.com>
2025-09-09 19:24:03 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,22 @@
+var typed = new Typed("#typed-text", {
+      strings: ["Web Developer", "Web Designer", "Freelancer", "Gamer"],
+      typeSpeed: 80,
+      backSpeed: 50,
+      backDelay: 1500,
+      loop: true
+    });
+
+    
+  const hamburger = document.querySelector("header button");
+  const mobileMenu = document.getElementById("mobileMenu");
+
+  hamburger.addEventListener("click", () => {
+    mobileMenu.classList.toggle("hidden"); // show/hide menu
+
+    // Optional: toggle hamburger icon to X
+    if (hamburger.textContent === "☰") {
+      hamburger.textContent = "✕"; // X mark
+    } else {
+      hamburger.textContent = "☰"; // Hamburger
+    }
+  });
